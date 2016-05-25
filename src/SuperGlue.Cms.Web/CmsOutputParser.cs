2450fec077747cdc55dc7a6c69a72c2ed96152f5
Mattias Jakobsson <mattias.jakobsson1988@gmail.com>
2016-05-25 20:51:23 +0200

    [FEATURE] Added configuration option to turn off transformation for some requests

--- src/SuperGlue.Cms.Web/CmsOutputParser.cs
+++ src/SuperGlue.Cms.Web/CmsOutputParser.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Threading.Tasks;
 using SuperGlue.Cms.Rendering;
+using SuperGlue.Configuration;
 using SuperGlue.Web.Output;
 
 namespace SuperGlue.Cms.Web
@@ -17,6 +18,14 @@ namespace SuperGlue.Cms.Web
 
         public async Task<OutputRenderingResult> Transform(OutputRenderingResult result, IDictionary<string, object> environment)
         {
+            var shouldTransform = await (environment.GetSettings<CmsParserSettings>().ShouldTransform ?? ((x, y) => Task.FromResult(true)))(result, environment).ConfigureAwait(false);
+
+            if (!shouldTransform)
+            {
+                environment.Log("Skipping transformation", LogLevel.Debug);
+                return result;
+            }
+
             using (var reader = new StreamReader(result.Body))
             {
                 result.Body.Position = 0;
---
+++ src/SuperGlue.Cms.Web/CmsParserSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SuperGlue.Web.Output;
+
+namespace SuperGlue.Cms.Web
+{
+    public class CmsParserSettings
+    {
+        public Func<OutputRenderingResult, IDictionary<string, object>, Task<bool>> ShouldTransform { get; private set; }
+
+        public void DetermineWhatToTransform(Func<OutputRenderingResult, IDictionary<string, object>, Task<bool>> shouldTransform)
+        {
+            ShouldTransform = shouldTransform;
+        }
+    }
+}
